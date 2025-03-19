c1f5a3f64ef5991949771dca914a827154eb8861
pop <popkter@outlook.com>
2025-03-19 15:48:55 +0800

    voice clone

---
+++ voice_clone.py
@@ -0,0 +1,54 @@
+import base64
+import os
+import requests
+
+host = "https://openspeech.bytedance.com"
+
+appid = "3550589109"
+token = "kr81m0XVOah3zSMi3PZHFfKt7UU1scxU"
+spk_id = "S_hWismBig1"
+
+# upload custom voice
+def train(audio_path):
+    url = host + "/api/v1/mega_tts/audio/upload"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer;" + token,
+        "Resource-Id": "volc.megatts.voiceclone",
+    }
+    encoded_data, audio_format = encode_audio_file(audio_path)
+    audios = [{"audio_bytes": encoded_data, "audio_format": audio_format}]
+    data = {"appid": appid, "speaker_id": spk_id, "audios": audios, "source": 2, "language": 0, "model_type": 1}
+    response = requests.post(url, json=data, headers=headers)
+    print("status code = ", response.status_code)
+    if response.status_code != 200:
+        print(response.json())
+        raise Exception("train请求错误:" + response.text)
+    print("headers = ", response.headers)
+    print(response.json())
+
+
+def get_status():
+    url = host + "/api/v1/mega_tts/status"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer;" + token,
+        "Resource-Id": "volc.megatts.voiceclone",
+    }
+    body = {"appid": appid, "speaker_id": spk_id}
+    response = requests.post(url, headers=headers, json=body)
+    print(response.json())
+
+
+def encode_audio_file(file_path):
+    with open(file_path, 'rb') as audio_file:
+        audio_data = audio_file.read()
+        encoded_data = str(base64.b64encode(audio_data), "utf-8")
+        audio_format = os.path.splitext(file_path)[1][1:]  # 获取文件扩展名作为音频格式
+        return encoded_data, audio_format
+
+
+if __name__ == "__main__":
+    train(audio_path="voice_demo.mp3")
+    print("check")
+    get_status()
